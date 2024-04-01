c4729a231ec8a9cf49cb46e388105a3bcc00bb99
Joel Odom <joel.odom@mongodb.com>
2024-04-01 11:21:10 -0400

    This commit is enough to demonstrate the  failure with automatic encryption. Notice that I have turned automatic encryption off as it's checked in.

--- python-client/lookup-experiment.py
+++ python-client/lookup-experiment.py
@@ -4,7 +4,35 @@ Another Joel Odom toy program to experiment with QE in MongoDB.
 
 import random
 import os
+
 from pymongo import MongoClient
+from pymongo.encryption_options import AutoEncryptionOpts
+
+#
+# Set up everything we will need for implicit encryption
+#
+
+KMS_PROVIDER_NAME = "local"  # instead of a cloud provider
+KEY_VAULT_NAMESPACE = "encryption.__keyVault"
+
+# 96 random hardcoded bytes, because it's only an example
+LOCAL_MASTER_KEY = b";1\x0f\x06%\x97\x99\xa5\xaen\xb4\x8b<T3v\x0b\\\xeb\x9f\x13\xa8\xb9\xc0[\xa0\xc3\xb9\xa7\x0e|\x8e3o5\x1a\xd8\x08H\x0b \xf1\xc1Eb\xeb\x0b\x8e\xde\xe4Oz\xe3\x0bs%$R\x13?\x9aI\x1d\xd0'\xee\xd8\x06\x85\x16\x90\xb0\x9ec#\x9c=Y\x8f\xc5\xc211\xc5\x15\x07\xae\xd2\xc6\xdb\xc5\x9c^S\xae,"
+
+KMS_PROVIDER_CREDENTIALS = {
+    "local": {
+        "key": LOCAL_MASTER_KEY
+    },
+}
+
+CRYPT_SHARED_LIB = "/Users/joel.odom/mongo_crypt_shared_v1-macos-arm64-enterprise-7.0.6/lib/mongo_crypt_v1.dylib"
+
+auto_encryption_options = AutoEncryptionOpts(  # use automatic encryption
+    KMS_PROVIDER_CREDENTIALS,
+    KEY_VAULT_NAMESPACE,
+    crypt_shared_lib_path=CRYPT_SHARED_LIB
+)
+
+auto_encryption_options = None  # Remove this to demonstrate the $lookup failure
 
 #
 # Connect to the Mongo database
@@ -14,7 +42,7 @@ PASSWORD=os.getenv("JOEL_ATLAS_PWD")
 if PASSWORD is None:
     raise Exception("Password not set in environment.")
 URI = f"mongodb+srv://joelodom:{PASSWORD}@joelqecluster.udwxc.mongodb.net/?retryWrites=true&w=majority&appName=JoelQECluster"
-MONGO_CLIENT = MongoClient(URI)
+MONGO_CLIENT = MongoClient(URI, auto_encryption_opts=auto_encryption_options)
 DB_NAME = "lookup-experiment"
 DB = MONGO_CLIENT[DB_NAME] # This alone is not enough to create a database
 
