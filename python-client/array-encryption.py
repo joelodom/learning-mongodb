6a92c6174a517bca4f94eae9b8d3f2a955b13d99
Joel Odom <joel.odom@mongodb.com>
2024-04-10 14:18:58 -0400

    Added comments to explain array experiment.

--- python-client/array-encryption.py
+++ python-client/array-encryption.py
@@ -1,5 +1,7 @@
 """
 This is a toy program by Joel Odom to demonstrate MongoDB CSFLE encryption and arrays.
+
+As of 7.0 we don't have support for CSFLE and arrays.
 """
 
 import os
@@ -9,27 +11,28 @@ from pymongo.encryption import ClientEncryption
 from pymongo.encryption_options import AutoEncryptionOpts
 
 #
-# TODO: Add good comments throughout
+# Connect to my Atlas instance, create a ClientEncryption helper, and create the
+# data key.
 #
 
 PASSWORD = os.getenv("JOEL_ATLAS_PWD")
 if PASSWORD is None:
     raise Exception("Password not set in environment.")
 URI = f"mongodb+srv://joelodom:{PASSWORD}@joelqecluster.udwxc.mongodb.net/?retryWrites=true&w=majority&appName=JoelQECluster"
-DB_NAME = "array-experiment"
 
 # 96 random hardcoded key bytes, because it's only an example
 LOCAL_MASTER_KEY = b";1\x0f\x06%\x97\x99\xa5\xaen\xb4\x8b<T3v\x0b\\\xeb\x9f\x13\xa8\xb9\xc0[\xa0\xc3\xb9\xa7\x0e|\x8e3o5\x1a\xd8\x08H\x0b \xf1\xc1Eb\xeb\x0b\x8e\xde\xe4Oz\xe3\x0bs%$R\x13?\x9aI\x1d\xd0'\xee\xd8\x06\x85\x16\x90\xb0\x9ec#\x9c=Y\x8f\xc5\xc211\xc5\x15\x07\xae\xd2\xc6\xdb\xc5\x9c^S\xae,"
 
-kms_providers = {
+kms_providers = {  # This is where the ClientEncryption will fetch keys
     "local": {
         "key": LOCAL_MASTER_KEY
     },
 }
 
-KEY_VAULT_DB = "array-encryption-keys"
+KEY_VAULT_DB = "array_encryption_keys"  # all encrypted with the CMK
 KEY_VAULT_COLL = "__keyVault"
 key_vault_namespace = f"{KEY_VAULT_DB}.{KEY_VAULT_COLL}"
+
 key_vault_client = MongoClient(URI)
 
 client_encryption = ClientEncryption(
@@ -44,6 +47,10 @@ client_encryption = ClientEncryption(
 # run of the program (see the quick start guide for more about this).
 data_key_id = client_encryption.create_data_key("local")
 
+#
+# Now create a database schema that specifies what to encrypt and how.
+#
+
 DATABASE = "array_encryption_database"
 COLLECTION = "array_encryption_collection"
 
@@ -53,14 +60,20 @@ schema_map = {
         "properties": {
             "encryptedArray": {
                 "encrypt": {
-                    "keyId": [data_key_id],
-                    "bsonType": "array",  # TODO: change to array
+                    "keyId": [ data_key_id ],
+                    "bsonType": "array",
                     "algorithm": "AEAD_AES_256_CBC_HMAC_SHA_512-Deterministic"
                 }
             }
         }
     }
 }
+
+#
+# Create a record in the database that includes an encrypted array. This uses
+# a seprate MongoClient because the steps above are just for creating
+# a key database and wouldn't be done again in a real program.
+#
 
 client = MongoClient(
     URI,
@@ -78,3 +91,8 @@ doc = {
     }
 
 client[DATABASE][COLLECTION].insert_one(doc)
+
+#
+# In 7.0 the line above fails with, "Cannot encrypt element of type: array" because
+# we don't support it yet.
+#
