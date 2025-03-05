57e5117336d1e3197b69c7d0bdb3082c63ea79d0
Joel Odom <joel.odom@mongodb.com>
2025-03-05 08:10:34 -0500

    The Java work is a WIP.

---
+++ java-client/src/main/java/com/joelodom/qe/JoelQEDemo.java
@@ -0,0 +1,7 @@
+package com.joelodom.qe;
+
+public class JoelQEDemo {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Queryable Encryption Demonstration");
+    }
+}
